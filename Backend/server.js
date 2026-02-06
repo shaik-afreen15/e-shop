da288598bde73a309563cc600fee24446897cbd0
Shaik Afreen <shaikafreen1507@gmail.com>
2026-02-06 14:34:16 +0530

    axios.js

--- Backend/server.js
+++ Backend/server.js
@@ -41,7 +41,11 @@ app.use("/api/products", productRoutes);
 app.use("/api/profile", profileRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+
+export default app;
+
 
