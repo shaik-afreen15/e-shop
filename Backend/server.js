8dba5242c9105527599cdef05541cf2dd3812688
Shaik Afreen <shaikafreen1507@gmail.com>
2026-02-07 12:18:16 +0530

    server.js

--- Backend/server.js
+++ Backend/server.js
@@ -22,6 +22,20 @@ app.use(
   })
 );
 
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,DELETE,PATCH,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  return res.sendStatus(204);
+});
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
